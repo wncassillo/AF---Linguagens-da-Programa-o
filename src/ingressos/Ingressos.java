1391003e16eaff3a617e182c5c6bb376f9218191
wncassillo <cassilloneto@gmail.com>
2022-11-29 21:58:13 -0300

    primeiro commit

---
+++ src/ingressos/Ingressos.java
@@ -0,0 +1,18 @@
+package ingressos;
+import view.SelectionView;
+
+/**
+ * @author wncassillo
+ */
+public class Ingressos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        view.SelectionView selection = new SelectionView();
+        selection.setVisible(true);
+    }
+    
+}
