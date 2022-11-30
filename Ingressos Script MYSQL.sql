6ede48bd55f215263ce3920aab482a565a6060c2
wncassillo <cassilloneto@gmail.com>
2022-11-30 16:52:56 -0300

    Terceiro Commit

---
+++ Ingressos Script MYSQL.sql
@@ -0,0 +1,13 @@
+drop database if exists Ingressos;
+create database Ingressos;
+use Ingressos;
+
+create table Vendas(
+id			int				primary key		auto_increment,
+nome 		varchar(45)		not null,
+cpf 		varchar(15)		not null,
+telefone 	varchar(12)		not null, 
+ingresso	varchar(10)		not null, 
+assento		varchar(4)		not null,
+pagamento 	varchar(18)		not null 
+);
